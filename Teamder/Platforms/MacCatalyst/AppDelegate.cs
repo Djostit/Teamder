08b05b3568dee2d1beb2e616b51b44131db839cc
Djostit <djostit@mail.ru>
2023-01-30 16:22:50 +0500

    Добавьте файлы проекта.

---
+++ Teamder/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Teamder;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
