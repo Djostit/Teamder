1986d895118146106d35fde60fa4f4d6c9c80f8e
Djostit <djostit@mail.ru>
2023-02-01 10:41:39 +0500

    Add command (#10)

--- Teamder/ViewModels/SingInVM.cs
+++ Teamder/ViewModels/SingInVM.cs
@@ -1,14 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Teamder.Services;
 
 namespace Teamder.ViewModels
 {
     public partial class SingInVM : BaseViewModel
     {
+        private readonly UserService _userService;
         [ObservableProperty]
         [NotifyCanExecuteChangedFor(nameof(SignInCommand))]
         public string login;
@@ -21,16 +24,19 @@ namespace Teamder.ViewModels
         public string logging = "false";
         public bool CanSignIn  => !string.IsNullOrWhiteSpace(Login) && !string.IsNullOrWhiteSpace(Password);
 
-        public SingInVM()
+        public SingInVM(UserService userService)
         {
+            _userService = userService;
             Logging = "false";
         }
 
         [RelayCommand(CanExecute = "CanSignIn")]
-        private void SignIn()
+        private async Task SignIn()
         {
             Logging = "true";
-            Shell.Current.DisplayAlert("Debug", Login + "\n" + Password, "ok");
+            //Shell.Current.DisplayAlert("Debug", Login + "\n" + Password, "ok");
+
+            await _userService.AuthorisationUserAsync(Login, Password);
         }
     }
 }
