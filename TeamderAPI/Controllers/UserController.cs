98cbf9875733496f7218188a4881d858186d308f
Djostit <djostit@mail.ru>
2023-02-01 09:52:44 +0500

    Fix model and Add backend (#7)

---
+++ TeamderAPI/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeamderAPI.Data;
+using TeamderAPI.Models;
+
+namespace TeamderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Authorization")]
+        public async Task<ActionResult<User>> Authorization(User userr)
+        {
+            var user = await _context.User.SingleOrDefaultAsync(u => u.Username.Equals(userr.Username));
+            if (user == null)
+                return BadRequest("User not found");
+            if (!BCrypt.Net.BCrypt.Verify(userr.Password, user.Password))
+                return BadRequest("User not found");
+
+            return Ok(user);
+        }
+        [HttpPost("AddUser")]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            await _context.User.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.User.ToListAsync());
+        }
+    }
+}
